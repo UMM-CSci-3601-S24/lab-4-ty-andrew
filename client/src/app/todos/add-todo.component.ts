3cc23a876293310c23ea9e923a506c30a5dd3ed4
Andrew Lam <lam00139@morris.umn.edu>
2024-02-21 15:50:24 -0600

    implementing add todo component.ts

---
+++ client/src/app/todos/add-todo.component.ts
@@ -0,0 +1,111 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { TodoService } from './todo.service';
+import { MatButtonModule } from '@angular/material/button';
+import { MatOptionModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+    selector: 'app-add-todo',
+    templateUrl: './add-todo.component.html',
+    styleUrls: ['./add-todo.component.scss'],
+    standalone: true,
+    imports: [FormsModule, ReactiveFormsModule, MatCardModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatOptionModule, MatButtonModule]
+})
+export class AddUserComponent {
+
+  addTodoForm = new FormGroup({
+    owner: new FormControl('', Validators.compose([
+      Validators.required,
+      Validators.minLength(2),
+      Validators.maxLength(50),
+    ])),
+
+    body: new FormControl('', Validators.compose([
+      Validators.required,
+      Validators.minLength(2),
+      Validators.maxLength(500),
+    ])),
+
+    category: new FormControl('', Validators.compose([
+      Validators.required,
+      Validators.pattern('^(video games|software design|groceries|homework)$'),
+    ])),
+
+    status: new FormControl(undefined, Validators.compose([
+      Validators.required,
+      Validators.pattern('^(true|false)$'),
+    ])),
+  });
+
+  readonly addTodoValidationMessages = {
+    owner: [
+      {type: 'required', message: 'Owner is required'},
+      {type: 'minlength', message: 'Owner must have 2 characters long'},
+      {type: 'maxlength', message: 'Owner must be under 50 characters long'},
+    ],
+
+    status: [
+      {type: 'required', message: 'Status is required'},
+      {type: 'pattern', message: 'Status must be either true or false'}
+    ],
+
+    body: [
+      {type: 'required', message: 'Body is required'},
+      {type: 'minlength', message: 'Body must have 2 characters long'},
+      {type: 'maxlength', message: 'Body must be under 500 characters long'},
+    ],
+
+    category: [
+      { type: 'required', message: 'Category is required' },
+      { type: 'pattern', message: 'Category must be "video games", "software design", "groceries", "homework"' },
+    ]
+  };
+
+  constructor(
+    private todoService: TodoService,
+    private snackBar: MatSnackBar,
+    private router: Router) {
+  }
+
+  formControlHasError(controlName: string): boolean {
+    return this.addTodoForm.get(controlName).invalid &&
+      (this.addTodoForm.get(controlName).dirty || this.addTodoForm.get(controlName).touched);
+  }
+
+  getErrorMessage(owner: keyof typeof this.addTodoValidationMessages): string {
+    for(const {type, message} of this.addTodoValidationMessages[owner]) {
+      if (this.addTodoForm.get(owner).hasError(type)) {
+        return message;
+      }
+    }
+    return 'Unknown error';
+  }
+
+  submitForm() {
+    this.todoService.addTodo(this.addTodoForm.value).subscribe({
+      next: (newId) => {
+        this.snackBar.open(
+          `Added todo ${this.addTodoForm.value.owner}`,
+          null,
+          { duration: 2000 }
+        );
+        this.router.navigate(['/todos/', newId]);
+      },
+      error: err => {
+        this.snackBar.open(
+          `Problem contacting the server – Error Code: ${err.status}\nMessage: ${err.message}`,
+          'OK',
+          { duration: 5000 }
+        );
+      },
+    });
+  }
+
+}
