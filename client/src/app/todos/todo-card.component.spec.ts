95901e4d470cb529dab88594a5d5f5251382db5f
Andrew Lam <lam00139@morris.umn.edu>
2024-02-21 14:51:19 -0600

    implementing todo card componentspec.ts

--- client/src/app/todos/todo-card.component.spec.ts
+++ client/src/app/todos/todo-card.component.spec.ts
@@ -1,19 +1,35 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
-import { TodoCardComponent } from '../todo-card/todo-card.component';
+import { TodoCardComponent } from './todo-card.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
 
 describe('TodoCardComponent', () => {
   let component: TodoCardComponent;
   let fixture: ComponentFixture<TodoCardComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [TodoCardComponent]
-    })
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+    imports: [
+        BrowserAnimationsModule,
+        MatCardModule,
+        TodoCardComponent
+    ]
+})
     .compileComponents();
+  }));
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(TodoCardComponent);
     component = fixture.componentInstance;
+    component.todo = {
+      _id: 'blanche_id',
+      owner: 'Blanche',
+      status: true,
+      category: 'video games',
+      body: 'legend of zelda',
+
+    };
     fixture.detectChanges();
   });
 
@@ -21,3 +37,4 @@ describe('TodoCardComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
